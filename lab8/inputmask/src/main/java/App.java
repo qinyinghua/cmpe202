1b900c47055bd8f8b21e22fe69d8f245656fc068
qinyinghua <yinghua.qin@sjsu.edu>
2018-10-26 21:03:20 -0700

    lab8 submission

--- lab8/inputmask/src/main/java/App.java
+++ lab8/inputmask/src/main/java/App.java
@@ -3,7 +3,7 @@
 /**
  *  Main App Class for Displaying Screen.
  */
-public class App {
+public class App implements IKeyPadObserver{
 
     private Screen screen;
     private CreditCardNum num;
@@ -22,6 +22,15 @@ public class App {
         screen.addSubComponent(exp);
         screen.addSubComponent(cvc);
 
+        //implement observer pattern
+        ((IKeyPadSubject)num).attach((IKeyPadObserver)this);
+        ((IKeyPadSubject)exp).attach((IKeyPadObserver)this);
+        ((IKeyPadSubject)cvc).attach((IKeyPadObserver)this);
+        
+        //to handle X key
+        ((IKeyPadSubject)exp).attach((IKeyPadObserver)num);
+        ((IKeyPadSubject)cvc).attach((IKeyPadObserver)exp);
+        
         count = 0;
 
     }
@@ -39,9 +48,19 @@ public class App {
     }
 
     public void key(String ch) {
-        count++;
-        screen.key(ch, count);
+    	if(ch.length()==1 && (ch.toLowerCase().equals("x") || (ch.charAt(0)>=48 && ch.charAt(0)<=57) )) {
+	        count++;
+	        screen.key(ch, count);
+	        //System.out.printf("\ncount:%s; key:%s\n",count,ch);
+    	}
     }
+
+	@Override
+	public void keyEventUpdate(int numKeys, String key) {
+		if(numKeys>=0) count=numKeys;
+		//System.out.printf("\ncount:%s; key:%s\n",numKeys,key);
+		// TODO Auto-generated method stub
+	}
 
 }
 
